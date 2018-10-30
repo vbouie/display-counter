07584eaca38234778049848473ddf00207d9d189
Victoria Bouie <vbouie123@Victorias-MacBook-Air.local>
2018-10-30 18:05:08 -0400

    2

--- src/App.js
+++ src/App.js
@@ -3,28 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor () {
-    super () 
-    this.state = { counter: 0}
-    this.addCounter = this.addCounter.bind(this)
-  }
-
-  addCounter() {
-    this.setState({counter: this.state.counter + 1})
-  }
-
-componentDidMount () {
- fetch('/counter').then(function(x) {
-   return x.text() 
- }).then(function(counter){
-   console.log(counter)
-  this.setState({counter: counter})
- }.bind(this))
-}
+  
   render() {
-    return (
-      <div className="App">
-       {this.state.counter}
+    return (<div>
+      <h1>hello world!</h1>
       </div>
     );
   }
